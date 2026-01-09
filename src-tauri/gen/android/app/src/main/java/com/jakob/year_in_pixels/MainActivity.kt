7dfbf0097d273a611bf326dc5e07f543568d611e
Jakob Wonisch <jakob.wonisch@gmail.com>
2026-01-10 00:48:12 +0100

    add android project

---
+++ src-tauri/gen/android/app/src/main/java/com/jakob/year_in_pixels/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jakob.year_in_pixels
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
